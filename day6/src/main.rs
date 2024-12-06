96469e327e35f5046022da3d18947b5c6495a31a
SadMatte <mathias@jbr.dk>
2024-12-06 11:59:23 +0100

    Pushing to work on this in the weekend

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -4,7 +4,7 @@ fn find_guard(layout: &Vec<&str>) -> Vec<usize> {
     for (y_index, _y_plane) in layout.iter().enumerate() {
         for (x_index, x_position) in layout[y_index].chars().enumerate() {
             if x_position == '^' {
-                let guard_position: Vec<usize> = vec![x_index, y_index];
+                let guard_position: Vec<usize> = vec![y_index, x_index];
                 return guard_position;
             }
         }
@@ -12,13 +12,22 @@ fn find_guard(layout: &Vec<&str>) -> Vec<usize> {
     panic!("Guard not found");
 }
 
-fn guard_move_forward(guard_facing_direction: &str) -> &str {
+fn is_object_in_front_of_guard(guard_facing_direction: &str, guard_position: &Vec<usize>, layout: &Vec<&str>) -> bool {
+    true
+}
+
+//moves guard in the direction it's facing
+fn guard_move_forward(guard_facing_direction: &str, guard_position: &Vec<usize>) -> Vec<usize> {
     match guard_facing_direction {
-        "up" => ,
+        "up" => return vec![guard_position[0] - 1, guard_position[1]],
+        "right" => return vec![guard_position[0], guard_position[1] + 1],
+        "down" => return vec![guard_position[0] + 1, guard_position[1]],
+        "left" => return vec![guard_position[0], guard_position[1] - 1],
         _ => panic!("Guard facing unknown direction")
     }
 }
 
+//turns guard to the right of whereever it's facing
 fn guard_turn_right(guard_facing_direction: &str) -> &str {
     match guard_facing_direction {
         "up" => return "right",
@@ -53,6 +62,8 @@ fn main() {
     let mut guard_position: Vec<usize> = Vec::new();
 
     guard_position = find_guard(&layout);
+
+    if layout[guard]
 
     /* 
     //find guard on the 1 dimensional plane
