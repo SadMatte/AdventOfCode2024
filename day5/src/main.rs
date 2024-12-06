90f97ec74a30af0b0e8fabbe7fec0c0889b955f3
SadMatte <mathias@jbr.dk>
2024-12-06 09:50:34 +0100

    skipped 3 days I guess

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -33,35 +33,6 @@ fn main() {
     let input: String = fs::read_to_string("input.txt")
         .expect("File not found");
 
-    let input = "47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
-
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47";
-
     //split the input into 2 parts: the rules and instructions
     let parts: Vec<&str> = input.split("\n\n").collect();
 
