b7566206576762bfada4a2ec07ad0203b6743ac3
SadMatte <mathias@jbr.dk>
2024-12-09 14:46:07 +0100

    uuh

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,9 +1,20 @@
 use std::fs;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 struct Node {
-    x: usize,
-    y: usize
+    x: i64,
+    y: i64
+}
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+struct AntiNode {
+    x: i64,
+    y: i64
+}
+
+fn remove_duplicates(anti_nodes: &mut Vec<AntiNode>) {
+    let mut unique_anti_nodes = HashSet::new();
+    anti_nodes.retain(|anti_node| unique_anti_nodes.insert(anti_node.clone()));
 }
 
 fn main() {
@@ -23,23 +34,82 @@ fn main() {
 ............
 ............";
 
+    /*
+    ......#....#
+    ...#....0...
+    ....#0....#.
+    ..#....0....
+    ....0....#..
+    .#....A.....
+    ...#........
+    #......#....
+    ........A...
+    .........A..
+    ..........#.
+    ..........#.
+    */
+
+    //matrix of all chars
     let matrix: Vec<&str> = input.split("\n").collect();
 
-    let nodes_map: HashMap<char, Vec<Node>> = HashMap::new();
+    //create maps to keep track of nodes
+    let mut nodes_map: HashMap<char, Vec<Node>> = HashMap::new();
+    let mut anti_nodes_map: HashMap<char, Vec<AntiNode>> = HashMap::new();
 
     for (y_index, y_plane) in matrix.iter().enumerate() {
         for (x_index, node_name) in y_plane.chars().enumerate() {
             if node_name == '.' {
                 continue;
             }
-            match nodes_map.get(&node_name) {
-                Some(nodes) => {
-                    nodes.push(Node {y: y_index, x: x_index});
-                },
-                None => {
 
+            nodes_map.entry(node_name).or_insert(Vec::new()).push(Node {
+                y: y_index as i64,
+                x: x_index as i64
+            });
+        }
+    }
+
+    //go through all nodes' following nodes and if there is already an antinode there or it's out of bounds, then don't add it
+    for (node_name, nodes) in &nodes_map {
+        for (first_node_index, _node) in nodes.iter().enumerate() {
+            for second_node_index in first_node_index+1..nodes.len() {
+                println!("first y of {} number {} is {}", node_name, first_node_index, nodes[first_node_index].y);
+                println!("first x of {} number {} is {}", node_name, first_node_index, nodes[first_node_index].x);
+                println!("second y of {} number {} is {}", node_name, second_node_index, nodes[second_node_index].y);
+                println!("second x of {} number {} is {}", node_name, second_node_index, nodes[second_node_index].x);
+                let first_anti_node: AntiNode = AntiNode {
+                    y: nodes[first_node_index].y*2 - nodes[second_node_index].y,
+                    x: nodes[first_node_index].x*2 - nodes[second_node_index].x
+                };
+                let second_anti_node: AntiNode = AntiNode {
+                    y: nodes[second_node_index].y*2 - nodes[first_node_index].y,
+                    x: nodes[second_node_index].x*2 - nodes[first_node_index].x
+                };
+
+                //if antinode is out of bounds then don't add
+                if first_anti_node.y < matrix.len() as i64 && first_anti_node.x < matrix[0].len() as i64 && first_anti_node.y >= 0 && first_anti_node.x >= 0 {
+                    println!("Antinode accepted with position {}, {}\n", first_anti_node.x, first_anti_node.y);
+                    anti_nodes_map.entry(*node_name).or_insert(Vec::new()).push(first_anti_node);
+                }
+                if second_anti_node.y < matrix.len() as i64 && second_anti_node.x < matrix[0].len() as i64 && second_anti_node.y >= 0 && second_anti_node.x >= 0 {
+                    println!("Antinode accepted with position {}, {}\n", second_anti_node.x, second_anti_node.y);
+                    anti_nodes_map.entry(*node_name).or_insert(Vec::new()).push(second_anti_node);
                 }
             }
         }
     }
+
+    //remove duplicates that may have been made
+    for anti_nodes in anti_nodes_map.values_mut() {
+        remove_duplicates(anti_nodes);
+    }
+
+    //count anti nodes
+    let mut total_anti_nodes: usize = 0;
+    for anti_nodes in anti_nodes_map.values() {
+        total_anti_nodes += anti_nodes.len();
+        println!("{:?}", anti_nodes);
+    }
+    
+    println!("There are a total of {} anti nodes", total_anti_nodes);
 }
